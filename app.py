9090d62f157d35c341843faf6437bafd5fa010c2
HaroldDRF <95robots@gmail.com>
2019-11-02 17:01:59 -0700

    items setup

--- app.py
+++ app.py
@@ -1,62 +1,25 @@
 from flask import Flask, jsonify
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-stores = [
-    {
-        'name': 'My Wonderful Store',
-        'items': [
-            {
-            'name': 'My Item',
-            'price': 15.99
-            }
-        ]   
-    }
-]
+items = []
 
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
 
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
-
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-
-    return jsonify({'message': 'store not found'})
-
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
-            }
-            store['items'].append(new_item)
-            return jsonify({'items': store['items']})
-    return jsonify({'message': 'store not found'})
-
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
-    return jsonify({'message': 'store not found'})
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201
+    
 
 
+api.add_resource(Item, '/item/<string:name')
 
 app.run(port=5000)
