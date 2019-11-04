72f54fcc4a90915980a94c382188c3a3f2cc082a
HaroldDRF <95robots@gmail.com>
2019-11-03 23:55:51 -0700

    new test code

--- app.py
+++ app.py
@@ -1,61 +1,115 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required, current_identity
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
-from security import authenticate, identity
+from db import db
+from blacklist import BLACKLIST
+from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.secret_key = 'andy'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["JWT_BLACKLIST_ENABLED"] = True  # enable blacklist feature
+app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = [
+    "access",
+    "refresh",
+]  # allow blacklisting for access and refresh tokens
+app.secret_key = "jose"  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
-jwt = JWT(app, authenticate, identity)
 
-items = []
 
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank!"
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWTManager(app)
+
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(
+    identity
+):  # Remember identity is what we define when creating the access token
+    if (
+        identity == 1
+    ):  # instead of hard-coding, we should read from a file or database to get a list of admins instead
+        return {"is_admin": True}
+    return {"is_admin": False}
+
+
+# This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return (
+        decrypted_token["jti"] in BLACKLIST
+    )  # Here we blacklist particular JWTs that have been created in the past.
+
+
+# The following callbacks are used for customizing jwt response/error messages.
+# The original ones may not be in a very pretty format (opinionated)
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({"message": "The token has expired.", "error": "token_expired"}), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(
+    error
+):  # we have to keep the argument here, since it's passed in by the caller internally
+    return (
+        jsonify(
+            {"message": "Signature verification failed.", "error": "invalid_token"}
+        ),
+        401,
     )
 
-    @jwt_required()
-    def get(self, name):
-        return {'item': next(filter(lambda x: x['name'] == name, items), None)}
 
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': f"An item with name {name} already exists."}
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return (
+        jsonify(
+            {
+                "description": "Request does not contain an access token.",
+                "error": "authorization_required",
+            }
+        ),
+        401,
+    )
 
-        data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item
 
-    @jwt_required()
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return (
+        jsonify(
+            {"description": "The token is not fresh.", "error": "fresh_token_required"}
+        ),
+        401,
+    )
 
-    @jwt_required()
-    def put(self, name):
-        data = Item.parser.parse_args()
-        
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
 
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return (
+        jsonify(
+            {"description": "The token has been revoked.", "error": "token_revoked"}
+        ),
+        401,
+    )
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
 
-if __name__ == '__main__':
-    app.run(debug=True)  
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
+api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(UserLogout, "/logout")
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True)
