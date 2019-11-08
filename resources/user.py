05f01682280121640a07f9b5d1af937f06397f11
HaroldDRF <95robots@gmail.com>
2019-11-07 23:45:00 -0700

    error messages start

--- resources/user.py
+++ resources/user.py
@@ -4,8 +4,8 @@ from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
     jwt_refresh_token_required,
-   
-   
+    get_jwt_identity,
+    jwt_required,
     get_raw_jwt,
 )
 from models.user import UserModel
