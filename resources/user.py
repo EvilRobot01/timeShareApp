42ce39676cef7fb73b8f9d3759a33b43c8c3b760
HaroldDRF <95robots@gmail.com>
2019-12-05 22:14:49 -0700

    addign actiavtion

--- resources/user.py
+++ resources/user.py
@@ -19,6 +19,7 @@ USER_NOT_FOUND = "User not found."
 USER_DELETED = "User deleted."
 INVALID_CREDENTIALS = "Invalid credentials!"
 USER_LOGGED_OUT = "User <id={user_id}> successfully logged out."
+NOT_CONFIRMED_ERROR = "You have not confirmed registration, please check your email <{}>"
 
 user_schema = UserSchema()
 
@@ -65,10 +66,11 @@ class UserLogin(Resource):
         user = UserModel.find_by_username(user_data.username)
 
         if user and safe_str_cmp(user_data.password, user.password):
-            access_token = create_access_token(identity=user.id, fresh=True)
-            refresh_token = create_refresh_token(user.id)
-            return {"access_token": access_token, "refresh_token": refresh_token}, 200
-
+            if user.activated:
+                access_token = create_access_token(identity=user.id, fresh=True)
+                refresh_token = create_refresh_token(user.id)
+                return {"access_token": access_token, "refresh_token": refresh_token}, 200
+            return {"message": NOT_CONFIRMED_ERROR.format(user.username)}, 400
         return {"message": INVALID_CREDENTIALS}, 401
 
 
